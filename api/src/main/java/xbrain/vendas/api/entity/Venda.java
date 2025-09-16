87a7a70f5a1298a59c001124bbd12a842f5e6547
Joao Vitor <joaovitorsantos103@hotmail.com>
2025-09-16 17:33:40 -0300

    feat: entity

---
+++ api/src/main/java/xbrain/vendas/api/entity/Venda.java
@@ -0,0 +1,24 @@
+package xbrain.vendas.api.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+public class Venda {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate dataVenda;
+    private Double valor;
+    private Long vendedorId;
+    private String vendedorNome;
+}
