9f8f244d4ef9c78df93bfb23d73c116e0841ec3e
Joao Vitor <joaovitorsantos103@hotmail.com>
2025-09-19 00:03:09 -0300

    fix : correction in data processing

--- api/src/main/java/xbrain/vendas/api/dto/RelatorioVendedorResponse.java
+++ api/src/main/java/xbrain/vendas/api/dto/RelatorioVendedorResponse.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 
     @Getter
     @AllArgsConstructor
-    public class RelatorioVendedorDTO {
+    public class RelatorioVendedorResponse {
         private String nome;
         private Double totalVendas;
         private Double mediaDiaria;
