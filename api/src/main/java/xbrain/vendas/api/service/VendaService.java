850a8fa83ace0fa901e9f59320c43eeaeb525c84
Joao Vitor <joaovitorsantos103@hotmail.com>
2025-09-22 10:46:00 -0300

    fix : Separating methods

--- api/src/main/java/xbrain/vendas/api/service/VendaService.java
+++ api/src/main/java/xbrain/vendas/api/service/VendaService.java
@@ -3,10 +3,9 @@ package xbrain.vendas.api.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import xbrain.vendas.api.dto.VendaRequest;
-import xbrain.vendas.api.dto.VendedorRequest;
-import xbrain.vendas.api.dto.VendedorResponse;
+import xbrain.vendas.api.dto.RelatorioResponse;
+import xbrain.vendas.api.dto.VendaResponse;
 import xbrain.vendas.api.entity.Vendas;
-import xbrain.vendas.api.entity.Vendedor;
 import xbrain.vendas.api.repository.VendaRepository;
 import xbrain.vendas.api.repository.VendedorRepository;
 
@@ -17,30 +16,39 @@ import java.util.List;
 @RequiredArgsConstructor
 @Service
 public class VendaService {
+
     private final VendaRepository vendaRepository;
     private final VendedorRepository vendedorRepository;
 
-
-    public Vendas criarVenda(VendaRequest request) {
-        return vendaRepository.save(Vendas.of(request));
-    }
-
-    public Vendedor criarVendedor(VendedorRequest request) {
-        return vendedorRepository.save(Vendedor.of(request));
+    public VendaResponse criarVenda(VendaRequest request) {
+        var vendedor = vendedorRepository.findById(request.getIdVendedor())
+                .orElseThrow(() -> new RuntimeException("Vendedor não encontrado."));
+        var venda = vendaRepository.save(Vendas.of(request));
+        return VendaResponse.of(venda,vendedor);
     }
 
     public List<Vendas> listarTodasVendas() {
         return vendaRepository.findAll();
     }
 
-
-
-    public List<VendedorResponse> relatorio(LocalDate inicio, LocalDate fim) {
+    public List<RelatorioResponse> relatorio(LocalDate inicio, LocalDate fim) {
         return vendedorRepository.findAll().stream()
                 .map(vendedor -> {
-                    var totalVendas = vendaRepository.countByPeriodo(inicio, fim, vendedor.getId());
-                    return VendedorResponse.of(vendedor, totalVendas, 0.0);
+                    var totalVendas = buscarTotalVendas(vendedor.getId(), inicio, fim);
+                    var mediaDiaria = calcularMediaDiaria( vendedor.getId(), inicio, fim);
+                    return RelatorioResponse.of(vendedor, totalVendas, mediaDiaria);
                 })
                 .toList();
+    }
+
+    private Long buscarTotalVendas(Long vendedorId, LocalDate inicio, LocalDate fim) {
+        return vendaRepository.countByPeriodo(inicio, fim, vendedorId);
+    }
+
+    private double calcularMediaDiaria(Long vendedorId, LocalDate inicio, LocalDate fim) {
+        var soma = vendaRepository.sumByPeriodo(inicio, fim, vendedorId);
+        var dias = ChronoUnit.DAYS.between(inicio, fim) + 1;
+        var media = dias > 0 ? soma / dias : 0.0;
+        return Math.round(media * 100.0) / 100.0;
     }
 }
